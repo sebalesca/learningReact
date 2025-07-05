252f40366f6a9a479c22baacce787aeb1bf1253a
Sebastian <sebalesca83o@gmail.com>
2025-07-05 15:17:44 -0300

    Make layout responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  onMenuClick: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onMenuClick }) => {
   return (
     <AppBar position="static">
       <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={onMenuClick}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <span className="material-icons">menu</span>
+        </IconButton>
         <Typography variant="h6" component="div">
           Webportal
         </Typography>
